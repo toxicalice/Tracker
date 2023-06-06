f0c4b6063799441015da6570126c24e61ba14ea6
toxicalice <ad.digital@icloud.com>
2023-06-06 13:35:39 +0300

    add LSView

---
+++ Tracker/LaunchScreen.swift
@@ -0,0 +1,46 @@
+//
+//  LaunchScreen.swift
+//  Tracker
+//
+//  Created by Алиса Долматова on 05.06.2023.
+//
+
+import Foundation
+import WebKit
+import UIKit
+
+
+class LaunchScreen: UIViewController  {
+    
+    var uiImage:UIImageView!
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = UIColor(named: "LSBlue")
+        setupViews()
+    }
+    
+    
+    private func setupViews() {
+        
+        let logoImage = UIImage(named: "Logo")
+        
+        uiImage = UIImageView()
+        
+        view.addSubview(uiImage)
+        uiImage.translatesAutoresizingMaskIntoConstraints = false
+        uiImage.image = logoImage
+        
+        
+        NSLayoutConstraint.activate([
+            uiImage.widthAnchor.constraint(equalToConstant: 94),
+            uiImage.heightAnchor.constraint(equalToConstant: 91),
+            uiImage.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            uiImage.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            
+        ])
+        
+        
+        
+        
+    }
+}
